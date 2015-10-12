eba68663585feefebb3fcbc404cff8fc8e52b8cc
wanghongfucoder <1583505981@qq.com>
2015-10-12 08:54:42 +0800

    enum example

--- src/TestGender.java
+++ src/TestGender.java
@@ -7,9 +7,10 @@ public class TestGender
 		// TODO Auto-generated method stub
 		//通过Enum的valueOf方法获取指定枚举类的枚举值
      Gender g=Enum.valueOf(Gender. class,"FEMALE");
-      g.name="女";
-    //直接访问枚举值的属性值
-      System.out.println(g+"代表："+g.name);
+      g.setName("女");
+      System.out.println(g+"代表："+g.getName());
+      g.setName("男");
+      System.out.println(g+"代表"+g.getName());
 	}
 
 }
