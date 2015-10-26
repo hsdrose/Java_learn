011e2a48afcc30a03abbc92d13a51c2437f5a6c9
wanghongfucoder <1583505981@qq.com>
2015-10-26 21:07:52 +0800

    泛型重写

--- src/Apple.java
+++ src/Apple.java
@@ -43,6 +43,8 @@ public class Apple<T>
 	{
 		// 因为传给T形参的是String 实际类型，所以构造器的参数只能是String
 		Apple<String> a1 = new Apple<String>("苹果");
+		Apple   a3=new Apple(new String("haha"));
+		System.out.println(a3);
 		System.out.println(a1.getInfo());
 		Apple<Double>  a2=new Apple<Double>(5.67);
 		System.out.println(a2.getInfo()	);
