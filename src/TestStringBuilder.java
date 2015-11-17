353dba8d6b409e01ce4e1e03238f28f8bfc3ce7a
wanghongfucoder <1583505981@qq.com>
2015-11-17 17:51:08 +0800

    StringBuilder 测试

--- src/TestStringBuilder.java
+++ src/TestStringBuilder.java
@@ -2,17 +2,21 @@ import java.util.*;
 import java.lang.*;
 public  class TestStringBuilder
 {
+	public static void main(String[] args) {
 	StringBuilder sb=new StringBuilder();
     sb.append("java");
 	System.out.println(sb);
 	sb.insert(0,"hello");
 	System.out.println(sb);
-	sb.replace(5,6,',');
-	System.out.println(sb)
+	// sb.replace(5,6,',');
+	System.out.println(sb);
 	sb.delete(5,6);
 	System.out.println(sb);
 	sb.reverse();
 	System.out.println(sb);
-
-    
+	System.out.println(sb.length());
+	System.out.println(sb.capacity());
+	sb.setLength(5);
+    System.out.println(sb);
+  }
 }
