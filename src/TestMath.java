769b072fd35b297faa4d40a03f59166a48b7a15b
wanghongfucoder <1583505981@qq.com>
2015-11-17 22:51:30 +0800

    Math 测试

---
+++ src/TestMath.java
@@ -0,0 +1,12 @@
+import java.math.*;
+import java.util.*;
+class TestMath {
+	public static void main(String[] args) {
+		//三角函数
+		System.out.println("Math.toDegrees(1.57)"+Math.toDegrees(1.57));
+		System.out.println("Math.toRadians(90)"+Math.toRadians(90));
+		System.out.println("Math.acos(0.3)"+Math.acos(0.3));
+		System.out.println("Math.asin(0.8)"+Math.asin(0.8));
+		System.out.println("Math.atan(2.3)"+Math.atan(2.3));
+	}
+}
